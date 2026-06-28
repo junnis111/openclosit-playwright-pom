1aa3dd961ed0ea1fda2ce0c29bc0bd77c2b423d8
junnis111 <junnis111@gmail.com>
2026-06-28 22:14:21 +0500

    fix kebab-case issues

--- pages/login-page.js
+++ pages/login-page.js
@@ -23,7 +23,7 @@ export class LoginPage {
 
     async getToastMessage() {
         // Invalid Email or password.
-        return this.page.locator(loginLocators.toastMessage).textContent()
+        return await this.page.locator(loginLocators.toastMessage).textContent()
     }
 
     async accountCreationNavigation() {
